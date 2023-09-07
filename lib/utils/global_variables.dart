43decb2c6c00842def93c60eee7453cf6fdbf9a5
LokmanHossen <lokmanhossen230@gmail.com>
2023-09-08 01:34:24 +0600

    Update

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone_flutter/screens/add_post.dart';
+import 'package:instagram_clone_flutter/screens/feed_screen.dart';
 
 const webScreenSize = 600;
 
 const homeScreenItems = [
-  Text('Feed'),
+ FeedScreen(),
   Text('Search'),
   AddPostScreen(),
   Text('notificatin'),
